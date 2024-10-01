c0da4923314eae2b8ec85434f9310595d39339e3
Gabriel Cinti <160785027+gabriel-cinti@users.noreply.github.com>
2024-10-01 15:26:47 -0300

    Dependency alteration

--- lojavideogames/src/main/java/com/lojavideogames/lojavideogames/LojavideogamesApplication.java
+++ lojavideogames/src/main/java/com/lojavideogames/lojavideogames/LojavideogamesApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LojavideogamesApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LojavideogamesApplication.class, args);
-	}
-
+	
+    public static void main(String[] args) {
+        SpringApplication.run(LojavideogamesApplication.class, args);
+    }
 }
