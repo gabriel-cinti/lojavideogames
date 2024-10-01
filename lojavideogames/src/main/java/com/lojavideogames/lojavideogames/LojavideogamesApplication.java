1a47cefd16340f8de484a2b53a287485fcfc7403
Gabriel Cinti <160785027+gabriel-cinti@users.noreply.github.com>
2024-10-01 15:27:11 -0300

    Structure alteration

--- lojavideogames/src/main/java/com/lojavideogames/lojavideogames/LojavideogamesApplication.java
+++ lojavideogames/src/main/java/com/lojavideogames/lojavideogames/LojavideogamesApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LojavideogamesApplication {
-	
-    public static void main(String[] args) {
-        SpringApplication.run(LojavideogamesApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavideogamesApplication.class, args);
+	}
+
 }
